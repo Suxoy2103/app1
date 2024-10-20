7c4513733df414fd01077a971301c4ae633ba437
Dima <dimasushkevich@gmail.com>
2024-10-20 11:11:04 +0200

    The main app is ready. pages: index, about, delivery and contact

--- main/views.py
+++ main/views.py
@@ -4,12 +4,37 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME'
-        }  
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME"
+    }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас", 
+        "content": "О нас",
+        "text_on_page": "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+
+    return render(request, 'main/about.html', context)
+
+
+def contact(request):
+    context = {
+        "title": "Contact - Главная",
+        "content": "Как с нами можно связаться:",
+        "phone": "067-127-57-67"
+    }
+
+    return render(request, 'main/contact.html', context) 
+
+
+def delivery(request):
+    context = {
+        "title": "Delivery - Главная",
+        "content": "Доставка и оплата",
+    }
+
+    return render(request, "main/delivery_pay.html", context)
