170caacf79f1cd4b7a5de56b28a1985a6dd7e18b
Dima <dimasushkevich@gmail.com>
2024-10-22 20:33:27 +0200

    After adding fixtures for goods

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
+
+
+sys.stdout.reconfigure(encoding="utf-8")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
