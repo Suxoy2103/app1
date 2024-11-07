38f558217e9b5ca5ed517b58ab097c03996b5717
Dima <dimasushkevich@gmail.com>
2024-11-07 20:06:50 +0100

    Admin panel setup

--- carts/models.py
+++ carts/models.py
@@ -12,20 +12,30 @@ class CartQueryset(models.QuerySet):
     def total_quantity(self):
         if self:
             return sum(cart.quantity for cart in self)
-        
+
         return 0
 
 
 class Cart(models.Model):
 
-    user = models.ForeignKey(to=User, verbose_name=("Пользователь"), blank=True, null=True, on_delete=models.CASCADE)
-    product = models.ForeignKey(to=Products, verbose_name=("Товар"), on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+    user = models.ForeignKey(
+        to=User,
+        verbose_name=("Пользователь"),
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        to=Products, verbose_name=("Товар"), on_delete=models.CASCADE
+    )
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name="Количество")
     session_key = models.CharField(max_length=32, blank=True, null=True)
-    created_timestamp = models.DateTimeField(verbose_name="Дата обновления", auto_now_add=True)
+    created_timestamp = models.DateTimeField(
+        verbose_name="Дата обновления", auto_now_add=True
+    )
 
     class Meta:
-        db_table = 'cart'
+        db_table = "cart"
         verbose_name = "Корзина"
         verbose_name_plural = "Корзина"
 
@@ -35,4 +45,7 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self):
-        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+        if self.user:
+            return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+
+        return f"Анонимная корзина | Товар {self.product.name} | Количество {self.quantity}"
