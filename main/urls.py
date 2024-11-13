6c9dbaeb7d9e480ad06fa0795a3dff3960ddcf5a
Dima <dimasushkevich@gmail.com>
2024-11-13 17:51:55 +0100

    Class based views

--- main/urls.py
+++ main/urls.py
@@ -23,8 +23,8 @@ from main import views
 app_name = 'main'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-    path("delivery/", views.delivery, name="delivery"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
+    path("delivery/", views.DeliveryView.as_view(), name="delivery"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,41 +1,82 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
-def index(request):
-    context = {
-        "title": "Home - Главная",
-        "content": "Магазин мебели HOME",
-    }
+class IndexView(TemplateView):
+    template_name= "main/index.html"
 
-    return render(request, 'main/index.html', context)
-
-
-def about(request):
-    context = {
-        "title": "Home - О нас", 
-        "content": "О нас",
-        "text_on_page": "Текст о том почему этот магазин такой классный, и какой хороший товар."
-    }
-
-    return render(request, 'main/about.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = "Магазин мебели Home"
+        return context
 
 
-def contact(request):
-    context = {
-        "title": "Contact - Главная",
-        "content": "Как с нами можно связаться:",
-        "phone": "067-127-57-67"
-    }
+class AboutView(TemplateView):
+    template_name = "main/about.html"
 
-    return render(request, 'main/contact.html', context) 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        return context
+
+class ContactView(TemplateView):
+    template_name = "main/contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Contact - Главная"
+        context["content"] = "Как с нами можно связаться:"
+        context["phone"] = "067-127-57-67"
+        return context
+
+class DeliveryView(TemplateView):
+    template_name= "main/delivery_pay.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Delivery - Главная"
+        context["content"] = "Доставка и оплата"
+        return context
 
 
-def delivery(request):
-    context = {
-        "title": "Delivery - Главная",
-        "content": "Доставка и оплата",
-    }
+# def index(request):
+#     context = {
+#         "title": "Home - Главная",
+#         "content": "Магазин мебели Home",
+#     }
 
-    return render(request, "main/delivery_pay.html", context)
+#     return render(request, "main/index.html", context)
+
+
+# def about(request):
+#     context = {
+#         "title": "Home - О нас",
+#         "content": "О нас",
+#         "text_on_page": "Текст о том почему этот магазин такой классный, и какой хороший товар."
+#     }
+
+#     return render(request, 'main/about.html', context)
+
+
+# def contact(request):
+#     context = {
+#         "title": "Contact - Главная",
+#         "content": "Как с нами можно связаться:",
+#         "phone": "067-127-57-67"
+#     }
+
+#     return render(request, 'main/contact.html', context)
+
+
+# def delivery(request):
+#     context = {
+#         "title": "Delivery - Главная",
+#         "content": "Доставка и оплата",
+#     }
+
+#     return render(request, "main/delivery_pay.html", context)
